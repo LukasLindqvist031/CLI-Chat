0b893bb6f0326800e9935a6e9ee08e2b3f95e2ed
LukasLindqvist031 <115667263+LukasLindqvist031@users.noreply.github.com>
2025-02-12 22:04:01 +0100

    Created preconditions for joining, creating rooms and sending messages

--- Server/Services/ChatService.cs
+++ Server/Services/ChatService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CLI_Chat.Server.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace CLI_Chat.Server.Services
 {
-    internal class ChatService
+    public class ChatService
     {
+        private readonly Dictionary<string, ChatRoom> _chatRooms = new Dictionary<string, ChatRoom>();
+
+        public bool CreateRoom(string name)
+        {
+            if (!_chatRooms.ContainsKey(name))
+            {
+                _chatRooms[name] = new ChatRoom() {Name = name };
+                return true;
+            }
+            return false;
+        }
+
+        public bool JoinRoom(string roomName, User username)
+        {
+            if (_chatRooms.TryGetValue(roomName, out var room))
+            {
+                room.Users.Add(username);
+                return true;
+            }
+            return false;
+        }
+
+        public bool SendMessage(User sender, string roomName, string content)
+        {
+            if(_chatRooms.TryGetValue(roomName, out var room))
+            {
+                room.Messages.Add(new Message { Sender = sender, Content = content });
+                return true;
+            }
+            return false;
+        }
     }
 }
