7a963b018bf2be69f99988589dfb9e39d1f72a65
LukasLindqvist031 <115667263+LukasLindqvist031@users.noreply.github.com>
2025-02-12 18:08:54 +0100

    Started added server functionality

--- Server/Networking/Server.cs
+++ Server/Networking/Server.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using CLI_Chat.Server.Services;
+using System.Net;
 
 namespace CLI_Chat.Server.Networking
 {
-    internal class Server
+    public class Server
     {
+        private readonly TcpListener _listener;
+        private readonly ChatService _chatService;
+
+        public Server(string ipAdress, int port)
+        {
+            _listener = new TcpListener(IPAddress.Parse(ipAdress), port);
+            _chatService = new ChatService();
+        }
+
+        public async Task StartAsync()
+        {
+            _listener.Start();
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                var client = await _listener.AcceptTcpClientAsync();
+                //Additional functionality
+            }
+
+        }
     }
 }
